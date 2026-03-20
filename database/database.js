3ca56ee5f04ccfba3007d9ed730beee092b34dad
Marc <marc.robledo@gmail.com>
2026-03-20 20:23:41 +0100

    added preliminary database

---
+++ database/database.js
@@ -0,0 +1,217 @@
+const INTERNAL_DATABASE=
+[
+/* ---------------- Game Boy ---------------- */
+[ //0
+["Aladdin",0x283c58b7],
+["Asterix & Obelix",0x5143e227],
+["Bionic Commando",0x4c5b4547],
+["BurgerTime Deluxe",0x88219a49],
+["Bust-A-Move 2: Arcade Edition",0xb94724e6],
+["Casper",0xe4b30634],
+["Castlevania: The Adventure",0x6977c265],
+["Daffy Duck",0x13dd647d],
+["Darkwing Duck",0xa1d4c544],
+["Donkey Kong Land",0x49dc0d37],
+["Donkey Kong",0xf777a5d8,1],
+["DuckTales 2",0x169b00c1],
+["F-1 Race",0xab83bd70],
+["Fortified Zone",0xe3b59b7e],
+["Game Boy Camera",0x4640909f],
+["Game Boy Gallery: 5 Games in One",0x263fd152],
+["Gargoyle's Quest",0x0030e61f],
+["Gremlins 2: The New Batch",0x3579e297],
+["Karakuri Kengou Den: Musashi Road",0x01aee24b],
+["Kirby no Kirakira Kids",0x47f42f42],
+["Makai Toushi Sa-Ga",0x1953820f],
+["Mario no Picross",0x17533700],
+["Mega Man: Dr. Wily's Revenge",0x2ea4b976],
+["Mickey Mouse",0x4ca75d4d],
+["Mickey's Dangerous Chase",0xe72db762],
+["Moguranya",0x82fca204],
+["Mole Mania",0x2c36c74c],
+["Nekketsu Koukou Soccer-bu: World Cup Hen",0x50c2ada1],
+["Parodius Da!",0x1791e952],
+["Pocket Monsters: Midori",0x37ae8dc4],
+["Pokemon: Edicion Amarilla: Edicion Especial Pikachu",0x964b7a10],
+["Pokemon: Edicion Azul",0xd95416f9],
+["Pokemon: Edicion Roja",0xd8507d8a],
+["Rockman World 5",0xeeabd3c6],
+["Sa-Ga 2: Hihou Densetsu",0xf6cfcfb1],
+["Sa-Ga 3: Jikuu no Hasha",0x575d6d9d],
+["Seiken Densetsu",0xd771c1b6],
+["Snoopy: Magic Show",0x2b7a5034],
+["SolarStriker",0x11817103],
+["Spanky's Quest",0xe9ef8136],
+["Super Mario Land 2: 6 Golden Coins",0x635a9112],
+["Super Mario Land",0x2c27ec70],
+["Tail 'Gator",0xc5acce7c],
+["Takahashi Meijin no Bouken-jima II",0xf1071e08],
+["Takahashi Meijin no Bouken-jima III",0xf8fc0b41],
+["Tamagotchi",0xdbf1bd20],
+["Taz-Mania",0x22d07cf6],
+["Teenage Mutant Hero Turtles: Fall of the Foot Clan",0xed8a73fb],
+["Tetris 2",0xea19a9d9],
+["Tetris",0x46df91ad],
+["The Amazing Spider-Man",0x2a4eafe4],
+["The Legend of Zelda: Link's Awakening",0x7d1b6cd6],
+["The Smurfs Nightmare",0x8ef938c4],
+["The Smurfs Travel the World",0xcb2c7198],
+["The Smurfs",0x8b5bcde7],
+["Trax",0x4a38be7d],
+["Turok: Battle of the Bionosaurs",0x518c2d2f],
+["Wario Land: Super Mario Land 3",0x40be3889],
+["World Bowling",0x896e76a2],
+["Yoshi's Cookie",0x75336712],
+["Zelda no Densetsu: Yume o Miru Shima",0x39a6684e],
+],
+
+
+
+/* ------------- Game Boy Color ------------- */
+[ //1
+["Crazy Bikers",0x53e02b87],
+["Croc 2",0xc1d60129],
+["Donkey Kong Country",0xb1743477],
+["Game & Watch Gallery 2",0x969c8961],
+["Harry Potter and the Chamber of Secrets",0x136e9d0c],
+["Harry Potter and the Sorcerer's Stone",0x4fd8b7c5],
+["Lucky Luke",0x412fb57c],
+["Mario Tennis",0x0510d601],
+["Mega Man Xtreme 2",0x8fedb6d8],
+["Mega Man Xtreme",0x3a4d94d5,1],
+["Metal Gear Solid",0x16796957],
+["Mr. Driller",0xbdf9b05c],
+["Pokemon Pinball",0x39c432a4],
+["Pokemon Puzzle Challenge",0x8206b1ce],
+["Pokemon: Edicion Cristal",0xff0a6f8a],
+["Pokemon: Edicion Oro",0x3434a92b],
+["Pokemon: Edicion Plata",0x1d9faac5],
+["R-Type DX",0xfc1d4089],
+["RPG Tsukuru GB",0x0b614307],
+["Sabrina: The Animated Series: Zapped!",0x818f3af6],
+["Samurai Kid",0x44a9ddfb],
+["Snoopy Tennis",0x8c2d9b43],
+["Star Wars Episode I: Racer",0x0ebc5758],
+["Super Mario Bros. Deluxe",0xa4cd26ff],
+["Super Robot Pinball",0x6e330fcd],
+["Tetris DX",0x69989152],
+["The Adventures of the Smurfs",0xd0d3dfed],
+["The Legend of Zelda: Link's Awakening DX",0x97822948],
+["The Legend of Zelda: Oracle of Ages",0x5933e3fa],
+["The Legend of Zelda: Oracle of Seasons",0xdbac1357],
+["Tintin: Prisoners of the Sun",0xb2205d49],
+["Wario Land II",0x047bdf80],
+],
+
+
+
+/* ------------ Game Boy Advance ------------ */
+[ //2
+["Advance Wars 2: Black Hole Rising",0x5a09aae6,1],
+["Advance Wars",0x66fb29e9],
+["Breath of Fire II",0xff5e8e39],
+["Breath of Fire",0xa1c3165d],
+["Broken Sword: The Shadow of the Templars",0x3a23efd7],
+["Castlevania Double Pack",0xee495897],
+["Castlevania: Harmony of Dissonance",0x521b3091],
+["ChuChu Rocket!",0x8ab607c0],
+["Classic NES Series - Donkey Kong",0xf53d8b56],
+["Classic NES Series - Excitebike",0x67d9a2a6],
+["Classic NES Series - The Legend of Zelda",0x6d49cabf],
+["Crash & Spyro Super Pack Volume 3",0x0510b70f],
+["Densetsu no Stafy 2",0xea73eb54,1],
+["Densetsu no Stafy",0xfae94c8b],
+["DK: King of Swing",0xf93b73d9],
+["Donkey Kong Country 2",0xaef11223],
+["Donkey Kong Country 3",0x708a6168],
+["Donkey Kong Country",0x41d277fe],
+["Doom II",0x60cd3207],
+["Doom",0x58c641b3],
+["Dragon Ball: Advanced Adventure",0x6c135820],
+["Final Fantasy I & II: Dawn of Souls",0x7c9854c6],
+["Final Fantasy IV Advance",0x27f2bc67],
+["Final Fantasy Tactics Advance",0xba5de047],
+["Final Fantasy V Advance",0x7197117e],
+["Final Fantasy VI Advance",0xd15deba5],
+["Fire Emblem: The Sacred Stones",0xb3005195],
+["Fire Emblem",0x9decc754],
+["Golden Sun",0xc63008d6],
+["Harry Potter and the Prisoner of Azkaban",0xc0bccaad],
+["Kirby & the Amazing Mirror",0x4f07c618],
+["Kirby: Nightmare in Dream Land",0x3b7a7477],
+["Klonoa: Empire of Dreams",0x69492530],
+["Konami Krazy Racers",0xcfec0650],
+["Koro Koro Puzzle: Happy Panechu!",0x0bfe46e9],
+["Kurukuru Kururin",0xc4dab956],
+["Lunar Legend",0x885266a2],
+["Maniac Racers Advance",0x2412c5eb],
+["Mario vs. Donkey Kong",0xca030d61],
+["Medal of Honor: Infiltrator",0xf23150a4],
+["Metal Slug Advance",0x3806f4ae],
+["Metroid Fusion",0x974e46ab],
+["Metroid: Zero Mission",0xf1d92e63],
+["Pac-Man Collection",0xe1578d25],
+["Phantasy Star Collection",0x4d9c671b],
+["Pokemon Mystery Dungeon: Red Rescue Team",0xc1d18fe4],
+["Pokemon Pinball: Ruby & Sapphire",0xa53ac14b],
+["Pokemon: Edicion Esmeralda",0x8c4d3108],
+["Pokemon: Edicion Rojo Fuego",0x9f08064e],
+["Pokemon: Edicion Verde Hoja",0x2ca11d59],
+["Racing Gears Advance",0x49a88e52],
+["Rayman: 10th Anniversary",0x437e95b9],
+["Rayman: Hoodlums' Revenge",0xc221c419],
+["Rayman: Raving Rabbids",0x9914da23],
+["Rhythm Tengoku",0xa6cd88e1],
+["Shining Force: Resurrection of the Dark Dragon",0x07b06994],
+["Sonic Advance 2",0x89509891],
+["Super Mario Advance 2: Super Mario World",0xfcfef343],
+["Super Mario Advance 3: Yoshi's Island",0x639e9d3b],
+["Super Mario Advance 4: Super Mario Bros. 3",0x37141f32],
+["Super Mario Advance 4: Super Mario Bros. 3",0x8d84acfc],
+["Super Mario Advance",0x1e4c6d6a],
+["Sword of Mana",0x11a0af49],
+["Tales of Phantasia",0xfa172a4a],
+["The Legend of Zelda: A Link to the Past & Four Swords",0x5a164321],
+["The Legend of Zelda: The Minish Cap",0xe8637292],
+["V-Rally 3",0x02d20287],
+["WarioWare: Twisted!",0xcb4e844b],
+],
+
+
+
+/* ---------------- Game Gear --------------- */
+[], //3
+
+
+
+/* -------------- Master System ------------- */
+[], //4
+
+
+
+/* ------------- Neo Geo Pocket ------------- */
+[], //5
+
+
+
+/* ---------- Neo Geo Pocket Color ---------- */
+[ //6
+["Baseball Stars Color",0xffa7e88d,1],
+["Crush Roller",0xf20ff9f2],
+["Delta Warp",0xadd4fdff],
+["Puyo Pop (v1.06)",0x5c649d1e],
+["Sonic the Hedgehog: Pocket Adventure",0x356f0849],
+["The King of Fighters: Battle de Paradise",0x77e37bac],
+],
+
+
+
+/* ------------- TurboGrafx-16 -------------- */
+[], //7
+
+
+
+/* --------------- Atari Lynx --------------- */
+[], //8
+
+];
